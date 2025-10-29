f6d9a8d7ddc85457f9ff51cd4ee1c7543978ee28
sivanikrishh <sivanikrishna13@gmail.com>
2025-10-29 15:50:37 +0530

    day 2 bootcamp update

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
+
+#connecting to database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+from models import db
+db.init_app(app)
 
 ## This is what we were doing in mad1
 # @app.route('/')
@@ -17,4 +24,6 @@ class HelloWorld(Resource):
 api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create database tables for our data models
     app.run(debug=True)
